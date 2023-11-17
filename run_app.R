3e954aee6101053e02ed7e27a0fd2867899395e4
marcusolatoye <marcus.olatoye@simplot.com>
2023-11-17 10:06:12 -0700

    server and ui scripts updated with new fucntion, replication, and size of panel

--- run_app.R
+++ run_app.R
@@ -1,8 +1,9 @@
 library(shiny)
 
 # Set the working directory
-setwd("C:/Users/olatoyeo/OneDrive - J.R. Simplot Company/Documents/2023/Biochemistry")
+setwd("C:/Users/olatoyeo/OneDrive - J.R. Simplot Company/Documents/2023/Biochemistry/extraction_app/")
 
 # Run the Shiny app
 shinyApp(ui = source("ui.R")$value, server = source("server.R")$value)
+
 
